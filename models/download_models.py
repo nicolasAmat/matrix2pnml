270a6d10479e918e58be37275a990efa8e424e96
Nicolas Amat <namat@laas.fr>
2021-05-19 14:10:51 +0200

    Update URL

--- models/download_models.py
+++ models/download_models.py
@@ -11,7 +11,7 @@ from bs4 import BeautifulSoup
 
 
 def main():
-    base_url = "http://www.hippo.iee.uz.zgora.pl/index.php?v=g&s=n_a&site="
+    base_url = "http://www.hippo.iie.uz.zgora.pl/index.php?v=g&s=n_a&site="
     soup = BeautifulSoup(requests.get(base_url).content, 'html.parser')
     number_pages = int(soup.find_all('a')[-2].text)
     for page in range(1, number_pages + 1):
@@ -19,7 +19,7 @@ def main():
         for img in soup.find_all('img'):
             model = Path(img['src']).stem
             print("Downloading: {}".format(model))
-            matrix_url = "http://www.hippo.iee.uz.zgora.pl/download.php?file=pnh/{}.pnh".format(model)
+            matrix_url = "http://www.hippo.iie.uz.zgora.pl/download.php?file=pnh/{}.pnh".format(model)
             open('{}.pnh'.format(model), 'wb').write(requests.get(matrix_url, allow_redirects=True).content)
 
 
